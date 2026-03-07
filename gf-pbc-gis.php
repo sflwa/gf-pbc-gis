600a2ba433d859772bbe4f07886f48d709373b6b
Philip Levine <philip@southfloridawebadvisors.com>
2026-03-06 21:30:44 -0500

    Update gf-pbc-gis.php

--- gf-pbc-gis.php
+++ gf-pbc-gis.php
@@ -6,7 +6,7 @@
  * Version:           1.4.0
  * Requires at least: 5.8
  * Requires PHP:      7.4
- * Author:            Philip Levine
+ * Author:            South Florida Web Advisors
  * Author URI:        https://sflwa.com/
  * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
